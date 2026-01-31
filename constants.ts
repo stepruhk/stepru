806dba74442cf0b4fc55032dd481cb76d8b37fae
stepruhk <stepruhk@gmail.com>
2026-01-31 18:44:58 -0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,7 +1,26 @@
 // --- COPIEZ TOUT CE TEXTE ---
-export const INITIAL_RESOURCES: Resource[] = [];
+export const INITIAL_RESOURCES: Resource[] = [
+  {
+    "id": "e1z9sj54a",
+    "type": "other",
+    "title": "Plan de cours Hiver 2026",
+    "content": "",
+    "url": "https://ena01.uqam.ca/pluginfile.php/9725407/mod_resource/content/1/COM1068%20gr%2012%20-%20H26%20plan%20de%20cours.pdf",
+    "courseId": "1",
+    "dateAdded": "2026-01-31T23:43:20.089Z"
+  }
+];
 
 export const INITIAL_FLASHCARDS: Flashcard[] = [];
 
-export const INITIAL_EVERNOTE: EvernoteNote[] = [];
+export const INITIAL_EVERNOTE: EvernoteNote[] = [
+  {
+    "id": "zjjzygm0p",
+    "courseId": "1",
+    "title": "Calendrier des activités et lectures - COM1068 H26",
+    "url": "https://share.evernote.com/note/b8e273df-0aad-f740-9c90-24aea41e7197",
+    "category": "Divers",
+    "dateAdded": "2026-01-31T23:43:44.389Z"
+  }
+];
 // --- FIN DU TEXTE ---
